316761bcdad2877582101643e8b5c7bcabc9838a
grigger1 <44089532+grigger1@users.noreply.github.com>
2019-01-25 15:59:35 +0200

    Ver 0.2

--- bot.py
+++ bot.py
@@ -1,1 +1,27 @@
-import discord.py-0.16.12
+import requests
+import urllib
+import discord
+import os
+from discord.ext.commands import Bot
+
+image_directory = "C:/Users/kapihla/Pictures/Saved Pictures"
+
+AUTH_TOKEN = os.getenv('DISCORD_AUTH_TOKEN')
+
+MyBot = Bot(command_prefix=('$$'))
+
+
+@MyBot.command(pass_context=True)
+async def save_image(context, url, filename):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(image-directory + "/" + filename + ".jpg", 'wb') as image:
+            image.write(response.content)
+
+@MyBot.command(pass_context=True)
+async def load_image(context, filename):
+    with open(image_directory + "/" + filename + ".jpg", 'rb') as image:
+        await context.channel.send(image)
+
+
+MyBot.run(AUTH_TOKEN)
