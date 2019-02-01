4f2c3b49d9552d6c25220ca70e9666de2e9e4ac1
grigger1 <44089532+grigger1@users.noreply.github.com>
2019-02-01 15:28:42 +0200

    Ver 1.0

--- bot.py
+++ bot.py
@@ -6,22 +6,27 @@ from discord.ext.commands import Bot
 
 image_directory = "C:/Users/kapihla/Pictures/Saved Pictures"
 
-AUTH_TOKEN = os.getenv('DISCORD_AUTH_TOKEN')
+AUTH_TOKEN_FILE = open(".env", "r")
+AUTH_TOKEN = str(AUTH_TOKEN_FILE.readlines(1)[0])
+AUTH_TOKEN.replace("'", "")
 
 MyBot = Bot(command_prefix=('$$'))
+
 
 
 @MyBot.command(pass_context=True)
 async def save_image(context, url, filename):
     response = requests.get(url)
     if response.status_code == 200:
-        with open(image-directory + "/" + filename + ".jpg", 'wb') as image:
+        with open(image_directory + "/" + filename + ".jpg", 'wb') as image:
             image.write(response.content)
+            await context.channel.send(content="Saved " + filename + ".")
 
 @MyBot.command(pass_context=True)
 async def load_image(context, filename):
     with open(image_directory + "/" + filename + ".jpg", 'rb') as image:
-        await context.channel.send(image)
+        await context.channel.send(content = "Here it is.")
+        await context.channel.send(file = discord.File(image))
 
 
 MyBot.run(AUTH_TOKEN)
